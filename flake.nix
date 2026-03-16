4f07596f657ef28d86490eb3b225e0b46183719d
drzzln <drzzln@protonmail.com>
2026-03-15 21:09:11 -0400

    refactor: use substrate repo-flake.nix universal builder
    Replaces bespoke flake.nix boilerplate with declarative config via
    substrate's repo-flake.nix. All build logic, devShell tooling, and
    meta attributes derived from (language, builder) dispatch.

--- flake.nix
+++ flake.nix
@@ -10,22 +10,15 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, substrate, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system: let
-      pkgs = import nixpkgs { inherit system; };
-      mkGoTool = (import "${substrate}/lib/go-tool.nix").mkGoTool;
-    in {
-      packages.default = mkGoTool pkgs {
-        pname = "steampipe-plugin-akeyless";
-        version = "0.0.0-dev";
-        src = self;
-        vendorHash = "sha256-idKunT7bMe+kaLEKMS6r3vB1OI/xY8OnJAXSE3UmHwE=";
-        description = "Akeyless plugin for Steampipe - query Akeyless resources with SQL";
-        homepage = "https://github.com/pleme-io/steampipe-plugin-akeyless";
-      };
-
-      devShells.default = pkgs.mkShellNoCC {
-        packages = with pkgs; [ go gopls gotools ];
-      };
-    });
+  outputs = inputs: (import "${inputs.substrate}/lib/repo-flake.nix" {
+    inherit (inputs) nixpkgs flake-utils;
+  }) {
+    self = inputs.self;
+    language = "go";
+    builder = "tool";
+    pname = "steampipe-plugin-akeyless";
+    vendorHash = "sha256-idKunT7bMe+kaLEKMS6r3vB1OI/xY8OnJAXSE3UmHwE=";
+    description = "Akeyless plugin for Steampipe - query Akeyless resources with SQL";
+    homepage = "https://github.com/pleme-io/steampipe-plugin-akeyless";
+  };
 }
